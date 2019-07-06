630b28fcb0bb41c2f6aa5bff47a26fd2967624c7
penTaKIill <1505094452@qq.com>
2019-07-06 10:34:27 +0800

    多区域高光显示

---
+++ CIGA/Assets/Scripts/test.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public DarkEffect m_dark;
+    public GameObject m_target;
+    public GameObject m_targetTwo;
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.W))
+        {
+            m_dark.addLightItem(m_target.transform, 100);
+            m_dark.addLightItem(m_targetTwo.transform, 100);
+        }
+        
+    }
+}
