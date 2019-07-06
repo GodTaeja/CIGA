3aa6f7464a41c27412d1c38272bcf33367dc74dd
Eru <497896100@qq.com>
2019-07-06 10:41:14 +0800

    //moveBox

---
+++ CIGA/Assets/moveBox/moveBoxScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveBoxScript : MonoBehaviour
+{
+    public float moveLength;
+    public float moveSpeed;
+    public float oneMoveLifeTime;
+    
+
+
+    private GameObject selfGo;
+    private Vector3 startMovePos;
+
+    public void InitBoxData(float moveLength,float moveSpeed,float oneMoveLifeTime)
+    {
+
+    }
+
+
+    private void Awake()
+    {
+        selfGo = gameObject;
+
+    }
+    
+    private void Update()
+    {
+        
+    }
+    
+    private void FixedUpdate()
+    {
+        gameObject.transform.position += (Vector3.left * Time.fixedDeltaTime * moveSpeed);
+
+
+    }
+
+
+
+
+
+
+
+
+
+}
